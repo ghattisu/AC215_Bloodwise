1a1c5f7e347c6f7ed93bb7e17a2a7e0308b8f7b3
Siavash Raissi <siavash.raissi@tufts.edu>
2024-10-10 18:57:46 -0400

    changed docker-shell.sh directories, created chunks for LLM

--- docker-shell.sh
+++ docker-shell.sh
@@ -7,7 +7,7 @@ set -e
 export BASE_DIR=$(pwd)
 export PERSISTENT_DIR=$(pwd)/../persistent-folder/
 export SECRETS_DIR=$(pwd)/../secrets/
-export GCP_PROJECT="" # CHANGE TO YOUR PROJECT ID
+export GCP_PROJECT="bloodwise-ai" # CHANGE TO YOUR PROJECT ID
 export GOOGLE_APPLICATION_CREDENTIALS="/secrets/llm-service-account.json"
 export IMAGE_NAME="llm-rag-cli"
 
