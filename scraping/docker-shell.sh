06912778554c5bac6a28477ed23ed308d682cfd7
lucychencys <111405909+lucychencys@users.noreply.github.com>
2024-10-18 14:56:45 -0400

    changed scraping to upload result to gcp bucket

--- scraping/docker-shell.sh
+++ scraping/docker-shell.sh
@@ -5,7 +5,7 @@ set -e
 
 # Set vairables
 export BASE_DIR=$(pwd)
-export PERSISTENT_DIR=$(pwd)/../persistent-folder/
+# export PERSISTENT_DIR=$(pwd)/../persistent-folder/
 export SECRETS_DIR=$(pwd)/../secrets/
 export GCP_PROJECT="bloodwise-ai" # CHANGE TO YOUR PROJECT ID
 export GOOGLE_APPLICATION_CREDENTIALS="/secrets/llm-service-account.json"
@@ -21,11 +21,6 @@ docker build -t $IMAGE_NAME -f Dockerfile .
 # Run All Containers
 # docker compose run --rm --service-ports $IMAGE_NAME
 
-
-
-
-
-
 # # Build the image based on the Dockerfile
 # docker build -t $IMAGE_NAME -f Dockerfile .
 
@@ -33,7 +28,6 @@ docker build -t $IMAGE_NAME -f Dockerfile .
 docker run --rm --name $IMAGE_NAME -ti \
 -v "$BASE_DIR":/app \
 -v "$SECRETS_DIR":/secrets \
--v "$PERSISTENT_DIR":/persistent \
 -e GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS \
 -e GCP_PROJECT=$GCP_PROJECT \
 $IMAGE_NAME
