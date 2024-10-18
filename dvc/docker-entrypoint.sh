524b36b9b5e02a07c53055b726d77a875cee2d41
Xingli Yu <xinyu1250@gmail.com>
2024-10-18 15:38:23 -0400

    update instructions

--- dvc/docker-entrypoint.sh
+++ dvc/docker-entrypoint.sh
@@ -7,7 +7,7 @@ gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS
 mkdir -p /mnt/gcs_bucket
 gcsfuse --key-file=$GOOGLE_APPLICATION_CREDENTIALS $GCS_BUCKET_NAME /mnt/gcs_data
 echo 'GCS bucket mounted at /mnt/gcs_data'
-mkdir -p /app/dataset
-mount --bind /mnt/gcs_data/images /app/dataset
+mkdir -p /app/datasets
+mount --bind /mnt/gcs_data/images /app/datasets
 
 pipenv shell
